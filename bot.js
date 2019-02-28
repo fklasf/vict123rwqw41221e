381300134b6a74afd064caf138bb7e84d0fec529
fklasf <48089522+fklasf@users.noreply.github.com>
2019-02-28 13:17:57 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js"); // بكج
+const client = new Discord.Client({disableEveryone: true});
+
+client.on('message', message => {
+  var prefix = "!"
+    var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+if (message.content.startsWith(prefix + 's')) {
+                                       message.delete()
+  if (message.author.id !== '399250242136047619') return;
+client.user.setGame(argresult, "http://twitch.tv/Victoria");
+    message.channel.sendMessage(`**\`${argresult}\` :تم تغيير الحالة الى**`).then(msg => {msg.delete(7000)});
+
+    
+} 
+
+
+ });
+
+
+client.login(process.env.BOT_TOKEN);
